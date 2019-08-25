0d39a148ed194db4463a91528fb0bf05ed266337
Thomas Depke <tdepke@mymail.mines.edu>
2019-08-25 03:17:20 -0600

    Added sort-of working colored grid.

--- src/main.cpp
+++ src/main.cpp
@@ -20,13 +20,16 @@ float randomFloat(float min, float max) {
     return minMaxRange(mainRNG);
 }
 
+const Vector2u GRID_SIZE(10, 10);
+
 void buildTriangles() {
-    const Vector2u GRID_SIZE(10, 10);
     const Vector2f CANVAS_SIZE(500.0f, 500.0f);
     const Vector2f SQUARE_SIZE(CANVAS_SIZE.x / GRID_SIZE.x, CANVAS_SIZE.y / GRID_SIZE.y);
     
     points.clear();
     points.reserve((GRID_SIZE.x + 1) * (GRID_SIZE.y + 1));
+    colors.clear();
+    colors.reserve(GRID_SIZE.x * GRID_SIZE.y * 2);
     
     points.push_back(Vector2f(0.0f, 0.0f));    // Top row.
     for (unsigned int x = 1; x < GRID_SIZE.x; ++x) {
@@ -48,19 +51,24 @@ void buildTriangles() {
         points.push_back(Vector2f(SQUARE_SIZE.x * x, CANVAS_SIZE.y));
     }
     points.push_back(CANVAS_SIZE);
+    
+    const int numColors = GRID_SIZE.x * GRID_SIZE.y * 2;
+    for (int i = 0; i < numColors; ++i) {
+        colors.push_back(Color(static_cast<Uint32>(static_cast<double>(i) / numColors * UINT_MAX)));
+    }
 }
 
 int main() {
     mainRNG.seed(static_cast<unsigned long>(chrono::high_resolution_clock::now().time_since_epoch().count()));
     RenderWindow window(VideoMode(INITIAL_WINDOW_SIZE.x, INITIAL_WINDOW_SIZE.y), "LowPoly Loading...");
     window.setFramerateLimit(FRAMERATE_LIMIT);
-    /*ConvexShape triangle(3);
+    ConvexShape triangle(3);
     triangle.setPoint(0, Vector2f(randomFloat(0.0f, 900.0f), randomFloat(0.0f, 900.0f)));
     triangle.setPoint(1, Vector2f(randomFloat(0.0f, 900.0f), randomFloat(0.0f, 900.0f)));
     triangle.setPoint(2, Vector2f(randomFloat(0.0f, 900.0f), randomFloat(0.0f, 900.0f)));
-    triangle.setFillColor(Color::Green);*/
-    Vertex point;
-    point.color = Color::Green;
+    triangle.setFillColor(Color::Green);
+    //Vertex point;
+    //point.color = Color::Green;
     buildTriangles();
     int fpsCounter = 0;
     Clock perSecondClock;
@@ -81,9 +89,25 @@ int main() {
         
         ++fpsCounter;
         window.clear();
-        for (const Vector2f& pos : points) {
+        /*for (const Vector2f& pos : points) {
             point.position = pos;
             window.draw(&point, 1, Points);
+        }*/
+        unsigned int colorIndex = 0, pointIndex = 0;
+        while (colorIndex < colors.size()) {
+            triangle.setPoint(0, points[pointIndex]);
+            triangle.setPoint(1, points[pointIndex + 1]);
+            triangle.setPoint(2, points[pointIndex + 1 + GRID_SIZE.x]);
+            triangle.setFillColor(colors[colorIndex]);
+            window.draw(triangle);
+            
+            triangle.setPoint(0, points[pointIndex + 1]);
+            triangle.setPoint(1, points[pointIndex + 1 + GRID_SIZE.x]);
+            triangle.setPoint(2, points[pointIndex + 2 + GRID_SIZE.x]);
+            triangle.setFillColor(colors[colorIndex + 1]);
+            window.draw(triangle);
+            colorIndex += 2;
+            ++pointIndex;
         }
         window.display();
     }
